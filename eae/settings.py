70f5ae3019e36d840d2645e65c731b1e08febd19
Negusse Berhane <negusebf12@gmail.com>
2021-03-16 16:08:04 +0300

    Update settings.py

--- eae/settings.py
+++ eae/settings.py
@@ -206,8 +206,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home' # You can use the test page
 LOGOUT_REDIRECT_URL = 'home' #You can use the thanks page
