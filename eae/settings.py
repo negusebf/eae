da7389541b68cc9c5e6671fcc06ec990a57dd4c3
Negusse Berhane <negusebf12@gmail.com>
2021-03-16 15:17:16 +0300

    Update settings.py

--- eae/settings.py
+++ eae/settings.py
@@ -16,7 +16,7 @@ from django.utils.translation import gettext_lazy as _
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 
